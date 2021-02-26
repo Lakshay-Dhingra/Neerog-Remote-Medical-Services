9af9b71586fed5037fa037b00c0e9780693baf6a
abhishek3758 <singlaabhishek84@gmail.com>
2021-02-26 17:38:47 +0530

    abhishek commit 4

--- Neerog/main_app/urls.py
+++ Neerog/main_app/urls.py
@@ -8,13 +8,17 @@ urlpatterns = [
     path('Doctor/city/',views.list_of_city,name='city'),
     path('Doctor/Hospital',views.list_of_hospital,name="list_of_hospital"),
     path('profile/',views.profile,name='profile'),
+    path('verify/',views.verify,name='verify'),
+    path('verify_certificate/<str:id>',views.verify_certificate,name='verify_certificate'),
     path('index/', views.index, name='index'),
     path('select_speciality/',views.select_speciality,name="select_speciality"),
     path('select_speciality/date/',views.chosen_date,name="chosen_date"),
+    path('Appointment_Details/<str:appointment_id>',views.Appointment_Details,name='Appointment_Details'),
     path('book_appointment1/<str:speciality>',views.book_appointment1,name="book_appointment1"),
     path('Appointment_Details_Submission/',views.Appointment_Details_Submission,name='Appointment_Details_Submission'),
     path('Selected/<str:hospital_id>/',views.Selected_Service_Provider,name="Selected"),
     path('prescription',views.Add_Prescription,name="prescription"),
     path('submit_Prescription',views.submit_Prescription,name="submit_Prescription"),
     path('book_appointment/',views.Book_Appointment,name="book_appointment"),
+    path('search_appointments',views.search_appointments,name='search_appointments')
 ]
